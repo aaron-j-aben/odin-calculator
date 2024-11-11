6c151be82829efe91aad31c95f5eaf97ab404534
aaron-j-aben <aaron.jm.aben@gmail.com>
2024-11-11 14:57:26 -0500

    Implement 4 basic arithmetic operations

---
+++ calculator.js
@@ -0,0 +1,27 @@
+/**
+ * Odin Calculator
+ * By Aaron J Aben
+ * 
+ * Arithmetic calculator with GUI
+ */
+
+/* Basic Operations with any number type */
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(dividend, divisor) {
+    if (divisor === 0) {
+        return null;
+    }
+
+    return dividend / divisor;
+}
